bf548021fcd7b05f1014665b4bb5a06cc4873f05
wickinot <27719504+wickinot@users.noreply.github.com>
2018-08-15 20:17:00 +1000

    Added eventlistener and clickcounter

--- tickerTracker.js
+++ tickerTracker.js
@@ -1,1 +1,9 @@
-alert("connected");
+
+var square = document.querySelector(".square");
+
+square.addEventListener("click", addClicks, false);
+
+function addClicks( event ) {
+	event.target.textContent = event.detail;
+};
+
