e6cad8ef106998bf5444407e61f21c9849acac06
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-08-17 13:19:13 +1000

    minor changes

--- tickerTracker.js
+++ tickerTracker.js
@@ -1,9 +1,12 @@
 
 var square = document.querySelector(".square");
 
-square.addEventListener("click", addClicks, false);
+square.addEventListener("mousedown", addClicks); // display the click count inside the square
 
-function addClicks( event ) {
-	event.target.textContent = event.detail;
-};
+// function addClicks( event ) {
+// 	event.target.textContent = event.detail; //detail: consecutive clicks in a short amount of time.
+// };
 
+function addClicks(event) {
+	
+}
