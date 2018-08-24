7664784014ff3f97c87b34d93a09e40b99caa55c
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-08-24 22:46:59 +1000

    event listener and for loop working

--- tickerTracker.js
+++ tickerTracker.js
@@ -1,17 +1,42 @@
 var square = document.querySelector(".square");
 
-square.addEventListener("mousedown", function(){
+square.addEventListener("click", function(){
+	//start with empty array
+	tap();
 	//each click = 1 tap count
 
 	//stop when tap count = 50
 	//add 1 to an empty array
 	//sum of clicks 
+	//display sum
+
 	//equal the sum of clicks to the tap count
+
+})
+
+
+function tap() {
+  var arr = [];
+
+  for (i = 0; i < 5; i++) {
+    arr.push(1)
+  }
+  
+  alert(arr)
 }
 
-
 function tapCount(){
+// var nums = [100,300,400,60,40];
 
+// var sum = 0;
+
+// for(var i=0; i < nums.length; i++){
+
+//     sum += nums[i]
+
+}
+
+alert(sum);
 }
 
 function timer(){
@@ -23,5 +48,5 @@ function reset(){
 }
 
 function help(){
-	
+
 }
