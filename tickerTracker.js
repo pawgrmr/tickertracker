0aa0740b028369caaf3b17a938d8018e81dad198
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-09-03 17:07:40 +1000

    turned square to button & tidied up rest function

--- tickerTracker.js
+++ tickerTracker.js
@@ -4,12 +4,14 @@ var timer;
 var resetButton = document.querySelector("#reset");
 // var timer;
 
- square.addEventListener("click", function(){
- 	// breaths();
- 	countDown(30);
+square.addEventListener("click", function(){
+ 	breaths();
  });
 
-
+$("#square").on("click", function(){
+	countDown(30);
+	$(this).off();
+});
 
 // keep running this function until 30sec is complete
 function breaths() {
@@ -22,7 +24,7 @@ function breaths() {
 // display SSR - alert box 
 
 function countDown(count) {
-	if (count > 0) {
+	if (count > (-1)) {
 		var x = document.getElementById("timerDiv");
 		x.innerHTML = count;
 		timer = setTimeout (function() {countDown(count-1);}, 1000);
@@ -40,7 +42,8 @@ resetButton.addEventListener("click", function(){
 function reset() {	
    clearTimeout(timer)
    	var x = document.getElementById("timerDiv");
-	x.innerHTML = 0;// to stop the timer from running.
+	x.innerHTML = 30;
+	square.textContent = " ";// to stop the timer from running. // to reset sum to nil in square.
 }
 
 function help(){
