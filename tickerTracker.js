1eb7425a2eb34b3217dd01217692429f9df7a8f7
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-09-08 00:16:57 +1000

    completed JS

--- tickerTracker.js
+++ tickerTracker.js
@@ -5,43 +5,46 @@ var x = document.querySelector("#timerDiv");
 var resetButton = document.querySelector("#reset");
 var sum = document.querySelector("#sum");
 var clicks;
+var y;
 
 init();
-
 
 function init() {
 	squareListener();
 }
 
-
 function squareListener() {
 	square.addEventListener("click", function(){
-	 	breaths(0);
+			breaths();	
 	 });
-
+	
 	$("#square").on("click", function(){
-		countDown(5);
-		$(this).off();	
+		countDown(30);
+		$(this).off();
+	
 	});
 
 }
 
 
-function breaths(num) {
+function breaths() {
+	breaths>=0;
 	breath++;
 	square.textContent = parseInt(breath);
 	var clicks = square.textContent;
 	SRR = clicks * 2; //actual SRR
+
 }
 
-
 function countDown(count) {
-	if (count > (-1)) {
-		x.textContent = count;
+	let y = count;
+	if (y > (-1)) {
+		x.textContent = y;
+
 		timer = setTimeout (function() {countDown(count-1);}, 1000);
-		if (count == 0){
+		if (y == 0){
 			window.alert("Sleeping Respiratory Rate: " + SRR)
-			x.textContent = count; 
+			x.textContent = y; 
 			sum.textContent = SRR;
 			square.textContent = clicks;
 		}
@@ -49,18 +52,23 @@ function countDown(count) {
 }
 
 
+
 resetButton.addEventListener("click", function(){
 	location.reload();
+	// reset();
+	
 })
 
 // function reset() {	
 //    clearTimeout(timer)
-// 	location.reload();
-// 	// x.innerHTML = " ";
-// 	// sum.innerHTML = " ";
-// 	// breath = -1;
-// 	// square.textContent = " ";// to stop the timer from running.
-// }
+
+// // return timer to " "
+// 	x.innerHTML = " ";
+// 	sum.innerHTML = " ";
+// 	square.textContent = " ";// to stop the timer from running.
+// 	breath=0;
+
+//  }
 
 
 
